8254d756506aaedc7392c03911eeb39ff82dad24
lizb <loo4love90@gmail.com>
2026-04-17 18:49:35 +0900

    tools update 0417

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from '../components/hero/Hero'
+import Tools from '../components/tools/Tools'
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Tools/>
     </div>
   )
 }
