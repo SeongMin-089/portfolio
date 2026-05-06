69c765786be9fab3b16fe2bd9e8b9076afeb4c5f
unknown <a99701841@gmail.com>
2026-05-06 18:29:16 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Skills from "./pages/skills/Skills"
 import PlaceholderPage from "./pages/PlaceholderPage"
 import Blog from "./pages/blog/Blog"
 import  ThemeProvider from './context/ThemeProvider'
+import SkillsPages from "./pages/skills/SkillsPages"
 
 import Layout from './components/layout/Layout'
 
@@ -33,7 +34,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="projects" element={<ProjectsPages />} />
-            <Route path="skills" element={<Skills />} />
+            <Route path="skills" element={<SkillsPages />} />
             <Route path="blog" element={<Blog />} />
             <Route path="contact" element={<Contact />} />
             <Route path="privacy" element={<PlaceholderPage title="Privacy" />} />
